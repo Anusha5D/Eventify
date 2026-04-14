13686aa1c94355f6ee29d890021dbc80c3ddd964
Anusha5D <143547787+Anusha5D@users.noreply.github.com>
2026-04-15 03:09:48 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eventify-qf4e.onrender.com',
+    baseURL: 'https://eventify-qf4e.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
